f7d9fa585f92ff054c8fd150450ef40ca5424290
ashokide <kbnashok@gmail.com>
2025-05-20 00:21:59 +0530

    terraform code with docker provider

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+# Pulls the image
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+# Create a container
+resource "docker_container" "web_server" {
+  image = docker_image.nginx.image_id
+  name  = "my_web_server_container"
+
+  ports{
+    internal = 80
+    external = 8080
+  }
+}
